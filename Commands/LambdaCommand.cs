304e7ce3fc960c5f86a5b1546363c337ebc0c128
_khel <KaiiilHell@gmail.com>
2021-10-03 21:32:54 +0300

    Команды

---
+++ Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KHSortGUI.Commands
+{
+	internal class LambdaCommand : Base.Command
+	{
+		private readonly Action<object> _Execute;
+		private readonly Func<object, bool> _CanExecute;
+
+		public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			_Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+			_CanExecute = canExecute;
+		}
+
+		public override bool CanExecute(object parameter)
+		{
+			return _CanExecute.Invoke(parameter);
+		}
+
+		public override void Execute(object parameter) => _Execute(parameter);
+	}
+}
--- ViewsModel/VmMain.cs
+++ ViewsModel/VmMain.cs
@@ -7,13 +7,9 @@ namespace KHSortGUI.ViewsModel
 {
 	internal class VmMain : Base.ViewModel
 	{
-		private const string TEST = "sdasds";
-
-
-
 		#region Заголовок окна
 		///<summary>Заголовок окна</summary>
-		private string _title = TEST;
+		private string _title = "SKSortGUI";
 		public string Title
 		{
 			get => _title;
