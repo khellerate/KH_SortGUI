6e957863aed7efd01f3770d97fa9c6d3ab7d142e
_khel <KaiiilHell@gmail.com>
2021-10-03 20:07:23 +0300

    Разделение View | ViewModel + рефакторинг

--- ViewsModel/Base/ViewModel.cs
+++ ViewsModel/Base/ViewModel.cs
@@ -1,22 +1,40 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace KHSortGUI.ViewsModel.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+	internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+	{
+		#region Назначение свойств
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string PropertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
-        }
+		protected virtual void OnPropertyChanged(string property = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
 
-        protected virtual bool Set<T>(ref T field, T value, string PropertyName = null)
-        {
-            if (Equals(field, value)) return false;
-            field = value;
-            OnPropertyChanged(PropertyName);
-            return true;
-        }
-    }
+		protected virtual bool Set<T>(ref T field, T value, string property = null)
+		{
+			if (Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(property);
+			return true;
+		}
+		#endregion
+
+		#region Диструктор
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		private bool _disposed;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing || _disposed) return;
+			_disposed = true;
+			//Тут освобождаем ресурсы
+		}
+		#endregion
+	}
 }
---
+++ ViewsModel/VmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KHSortGUI.ViewsModel
+{
+	internal class VmMain : Base.ViewModel
+	{
+		private const string TEST = "sdasds";
+
+
+
+		#region Заголовок окна
+		///<summary>Заголовок окна</summary>
+		private string _title = TEST;
+		public string Title
+		{
+			get => _title;
+			set => Set(ref _title, value);
+		}
+		#endregion
+	}
+}
